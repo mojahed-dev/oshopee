20d8af810c15e8ee8953f83034dd434080c39f41
MOJAHED.ALO.HABIB <mojahed.alo.habib@gmail.com>
2024-06-12 14:17:19 +0800

    Done with the API. More functionality to add in the future. Will work for Frontend, reactjs for now.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const blogcategoryRouter = require('./routes/blogCatRoute');
 const brandRouter = require('./routes/brandRoute');
 const colorRouter = require('./routes/colorRoute');
 const couponRouter = require('./routes/couponRoute');
+const enqRouter = require('./routes/enqRoute');
 const cookieParser =  require("cookie-parser");
 const morgan  = require("morgan");
 
@@ -35,6 +36,7 @@ app.use('/api/blogcategory', blogcategoryRouter);
 app.use('/api/brand', brandRouter);
 app.use('/api/coupon', couponRouter);
 app.use('/api/color', colorRouter);
+app.use('/api/enquiry', enqRouter);
 
 app.use(notFound);
 app.use(errorHandler);
